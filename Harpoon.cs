edb5578091396b8762bb50cc332e6f5a0c30170c
Zhonyanya <andreevich10050@gmail.com>
2025-09-20 17:35:17 +0400

    Add files via upload

---
+++ Harpoon.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Harpoon : MonoBehaviour
+{
+    public float harpoonSpeed;
+    public Transform cameraOrientation;
+    private bool isThrown = false;
+    Rigidbody rb;
+    private void Throw()
+    {
+        Vector3 throwDirection;
+        rb.useGravity = true;
+        throwDirection = cameraOrientation.forward * harpoonSpeed;
+        rb.AddForce(throwDirection, ForceMode.Force);
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        rb.useGravity = false;
+    }
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Throw();
+        }
+    }
+}
