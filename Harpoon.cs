bf4802b9d8712a3cd7902f4ef831416637a5a557
Zhonyanya <andreevich10050@gmail.com>
2025-09-25 18:35:09 +0400

    Update Harpoon.cs
    коммит ради коммита

--- Harpoon.cs
+++ Harpoon.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//скрипт гавно ща другой сделaю
+//скрипт гaвно ща другой сделaю
 public class Harpoon : MonoBehaviour
 {
     public float harpoonSpeed;
@@ -35,6 +35,7 @@ public class Harpoon : MonoBehaviour
         }
     }
 }
+
 
 
 
